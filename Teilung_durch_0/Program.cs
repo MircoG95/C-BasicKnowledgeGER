3d409b771e961a6b1ac211ebe76517cf21cc7b4a
Mirco <91058439+MircoG95@users.noreply.github.com>
2023-01-27 17:30:24 +0100

    Add files via upload

---
+++ Teilung_durch_0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Teilung_durch_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double zahl1 = 1523654, zahl2 = 0, erg;
+
+            erg = zahl1 / zahl2;
+
+            Console.WriteLine("Ergebnis: " + erg);
+
+            //Ganzzahlen: Exception
+            //Gleitkomma-Zahlen: unendlich
+        }
+    }
+}
