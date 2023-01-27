ee1d008f174c09075772e0b8950f2b993b5e2f86
Mirco <91058439+MircoG95@users.noreply.github.com>
2023-01-27 17:29:51 +0100

    Add files via upload

---
+++ ZufallZahlen/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ZufallZahlen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random zufall = new Random();
+            int zahl;
+
+            for (int i = 0; i < 100; i++)
+            {
+                zahl = zufall.Next(1, 101);
+                
+                Console.WriteLine(zahl);
+            }
+        }
+    }
+}
