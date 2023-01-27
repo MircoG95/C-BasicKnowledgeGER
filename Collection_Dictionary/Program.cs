3f84b55203dbcf7a80b6b3baedc9a587efcf17f6
Mirco <91058439+MircoG95@users.noreply.github.com>
2023-01-27 17:26:20 +0100

    Add files via upload

---
+++ Collection_Dictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collection_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArr = { 15, 3, 6, 7 };
+
+            Console.WriteLine(intArr[2]);
+            Console.WriteLine();
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            dict.Add("Schlüssel1", 42);
+            dict.Add("Schlüssel2", 21); 
+            //dict.Add("Schlüssel1", 43);       Exception: Key muss einzigartig sein
+
+            Console.WriteLine(dict["Schlüssel2"]);
+
+            Dictionary<short, char> dict2 = new Dictionary<short, char>();
+            List<char> li = new List<char>();
+        }
+    }
+}
